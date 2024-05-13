d63849d7b4683212605d5ea7ad7b26c5d7d52e19
Papan Das <hexoindia@gmail.com>
2024-05-13 19:33:57 +0530

    second test

--- src/sum/src/lib.rs
+++ src/sum/src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct MathStuffSum {
+    pub sum: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+entrypoint!(process_instruction);
+ 
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+    msg!("Hello World Rust program to sum two numbers entrypoint");
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let accounts_iter = &mut accounts.iter();
+    let account = next_account_info(accounts_iter)?;
+
+    if account.owner != program_id {
+        msg!("Account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
     }
+
+    msg!("Debug output:");
+    msg!("Account ID: {}", account.key);
+    msg!("Executable?: {}", account.executable);
+    msg!("Lamports: {:#?}", account.lamports);
+    msg!("Debug output complete.");
+    
+    msg!("Adding 1 to sum...");
+
+    // Increment
+    let mut math_stuff_sum = MathStuffSum::try_from_slice(&account.data.borrow())?;
+    math_stuff_sum.sum += 1;
+    math_stuff_sum.serialize(&mut &mut account.data.borrow_mut()[..])?;
+
+    msg!("Current sum is now: {}", math_stuff_sum.sum);
+ 
+    Ok(())
 }
